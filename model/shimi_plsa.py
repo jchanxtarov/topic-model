be9e014e347f3a9e6965627e50699a8d8cf5d7ec
jchanxtarov <ryotaro.shimizu@ZTA20103.local>
2021-03-02 15:12:23 +0900

    [add] memo

--- model/shimi_plsa.py
+++ model/shimi_plsa.py
@@ -126,3 +126,5 @@ class PLSA(object):
         PzPi_z = self.Pi_z * self.Pz
         Pz_i = PzPi_z / np.sum(PzPi_z, axis=1).reshape(self.n_uniq_items, 1)
         return Pz_i
+
+    # TODO: calc AIC, BIC
